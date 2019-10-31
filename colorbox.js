40fb77540714a27eec3eb7e9117735b200489233
jswa <jswa123@gmail.com>
2019-10-31 18:44:10 +0300

    Update colorbox.js

--- colorbox.js
+++ colorbox.js
@@ -1,5 +1,5 @@
 // ColorBox v1.3.16 - a full featured, light-weight, customizable lightbox based on jQuery 1.3+
-// Copyright (c) 2011 Jack Moore - jack@colorpowered.com
+// Copyright (c) 2011 Jdack Moore - jack@colorpowered.com
 // Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
 (function ($, document, window) {
 	var
